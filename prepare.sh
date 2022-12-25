422e17c74e7106f96a0374e49e0ab17f55f638d0
James Liu <cyliustack@gmail.com>
2022-12-26 03:13:13 +0800

    add prepare.sh to prepare required packages

---
+++ prepare.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+function main () {
+	SUDO=""
+	if [[ "$(command -v sudo)" != "" ]] && [[ "$(whoami)" != "root"  ]];then
+		SUDO="sudo"
+	fi
+
+	if [[ "$(command -v apt-get)" != "" ]];then
+		$SUDO apt-get update -y 
+		$SUDO apt-get update --fix-missing 
+		$SUDO apt install -y curl
+	elif [[ "$(command -v zypper)" != "" ]];then
+		$SUDO zypper install -y curl
+	elif [[ "$(command -v dnf)" != "" ]];then
+		$SUDO dnf install -y curl
+	else
+		echo "Your OS is not supported."
+		exit -1
+	fi
+	$SUDO apt install -y gcc-aarch64-linux-gnu 
+
+}
+
+main $@
